2fc09202d600599445bbb09b4f23269285526888
Anfimov <olehanfimov@gmail.com>
2021-07-11 20:26:50 +0300

    add VerifyRemoteAddressIsPrivate middleware

--- server/router.go
+++ server/router.go
@@ -1,8 +1,9 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/anfimovoleh/ms-content-manager/server/middlewares"
 
 	"github.com/go-chi/chi"
 )
@@ -10,10 +11,15 @@ import (
 func Router() chi.Router {
 	router := chi.NewRouter()
 
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write(
-			[]byte(fmt.Sprintf(`{"component": "ms-content-manager"}`)),
-		)
+	privateAddressPool := middlewares.PrivateAddressPool()
+
+	router.Group(func(r chi.Router) {
+		r.Use(middlewares.VerifyRemoteAddressIsPrivate(privateAddressPool))
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write(
+				[]byte(`{"component": "ms-content-manager"}`),
+			)
+		})
 	})
 
 	return router
