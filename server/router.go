c3b06bf5fa1181b23abcc1940e6fa2672cc75ed8
Anfimov <olehanfimov@gmail.com>
2021-07-19 22:28:28 +0300

    add upload file handler

--- server/router.go
+++ server/router.go
@@ -3,6 +3,8 @@ package server
 import (
 	"net/http"
 
+	"github.com/anfimovoleh/ms-content-manager/server/handlers"
+
 	"github.com/anfimovoleh/ms-content-manager/config"
 
 	"github.com/go-chi/chi/middleware"
@@ -31,18 +33,22 @@ func Router(cfg config.Config) chi.Router {
 		middleware.Recoverer,
 	)
 
-	router.Mount("/admin", adminRouter())
+	router.Mount("/admin", adminRouter(cfg))
 
 	return router
 }
 
 // A completely separate router for administrator routes
-func adminRouter() http.Handler {
+func adminRouter(cfg config.Config) http.Handler {
 	r := chi.NewRouter()
 	r.Use(
 		middlewares.VerifyRemoteAddressIsPrivate(middlewares.PrivateAddressPool()),
 		middlewares.BasicAuth("admin", "admin"),
 	)
+
+	r.Route("/upload", func(r chi.Router) {
+		r.Post("/file", handlers.NewUploadDocumentHandler(cfg.Log()).Handle)
+	})
 
 	return r
 }
